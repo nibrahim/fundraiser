2bf71e605a5791514b5b6cb399552d619f085a1d
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-08-25 17:47:58 +0530

    Minor tweaks to give the application a whirl

--- fund_raiser/settings/local.py
+++ fund_raiser/settings/local.py
@@ -9,7 +9,7 @@ DB_PASSWORD = get_env_variable('DB_PASSWORD')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
